2d15bac61ca4af5478e2042315ce73a26e6dcd29
Darien <darien.menendez87@gmail.com>
2026-02-09 17:45:47 -0600

    Updates

---
+++ sw.js
@@ -0,0 +1,120 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
+
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "index.html",
+    "revision": "ad5fadfdf19bbf17cc8dfa99d5496bfb"
+  }, {
+    "url": "assets/workbox-window.prod.es5-BIl4cyR9.js",
+    "revision": null
+  }, {
+    "url": "assets/virtual_pwa-register-DGweFikl.js",
+    "revision": null
+  }, {
+    "url": "assets/index-D6nn8Exs.css",
+    "revision": null
+  }, {
+    "url": "assets/index-CHga6s_t.js",
+    "revision": null
+  }, {
+    "url": "FilTracker/icon.svg",
+    "revision": "dea70e444bbb10d168d51ae6598c3bb8"
+  }, {
+    "url": "FilTracker/icon-512.png",
+    "revision": "3e1b5668cd41eddee11428418e3562bd"
+  }, {
+    "url": "FilTracker/icon-192.png",
+    "revision": "74915024c0e9ca27427baaccd7797c34"
+  }, {
+    "url": "3d-printer-tracker/icon.svg",
+    "revision": "dea70e444bbb10d168d51ae6598c3bb8"
+  }, {
+    "url": "3d-printer-tracker/icon-512.png",
+    "revision": "3e1b5668cd41eddee11428418e3562bd"
+  }, {
+    "url": "3d-printer-tracker/icon-192.png",
+    "revision": "74915024c0e9ca27427baaccd7797c34"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
